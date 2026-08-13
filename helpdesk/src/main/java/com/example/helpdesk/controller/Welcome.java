30de6f24f74ded7aebcf94af0fee17a65247c964
Rodrigo <aluno.den@senaibahia.com.br>
2026-08-13 19:45:13 -0300

    Welcome Danilo

---
+++ helpdesk/src/main/java/com/example/helpdesk/controller/Welcome.java
@@ -0,0 +1,9 @@
+package com.example.helpdesk.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+public class Welcome {
+
+    @RestController
+    public class welcome
+}
