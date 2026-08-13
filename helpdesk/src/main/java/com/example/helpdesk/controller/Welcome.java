17fde768a06b8cf468dddd44e5aa6bb26d5cde16
Rodrigo <aluno.den@senaibahia.com.br>
2026-08-13 19:52:52 -0300

    iniciando projeto

--- helpdesk/src/main/java/com/example/helpdesk/controller/Welcome.java
+++ helpdesk/src/main/java/com/example/helpdesk/controller/Welcome.java
@@ -1,9 +1,23 @@
 package com.example.helpdesk.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-public class Welcome {
-
     @RestController
-    public class welcome
-}
+    public class Welcome {
+
+        @GetMapping("/")
+        public String mensagem() {
+            return "Sejam bem-vindos";
+        }
+
+        @GetMapping("/dev")
+        public String dev() {
+            return "Atividades retornadas";
+        }
+    }
+
+
+
+
