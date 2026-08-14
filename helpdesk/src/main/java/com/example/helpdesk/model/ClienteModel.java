06fd7394c4e7402f27813376cda888dc1e8881a2
Danilo Sena <arthur.cidreira@ba.estudante.senai.br>
2026-08-13 21:22:41 -0300

    Conserto model cliente e repository cliente

--- helpdesk/src/main/java/com/example/helpdesk/model/ClienteModel.java
+++ helpdesk/src/main/java/com/example/helpdesk/model/ClienteModel.java
@@ -17,4 +17,46 @@ public class ClienteModel {
 
     @Column(nullable = false)
     private String telefone;
+
+    public ClienteModel() {
+    }
+
+    public ClienteModel(Long id, String nome, String email, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }
